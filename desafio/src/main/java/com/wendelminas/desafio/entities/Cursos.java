bd8d3ad8a64dba85407afcc506e266ae547f6a08
Wendel Leite da Silva <136202512+wendelminas@users.noreply.github.com>
2023-06-22 11:03:10 -0300

    Integração com o github

--- desafio/src/main/java/com/wendelminas/desafio/entities/Cursos.java
+++ desafio/src/main/java/com/wendelminas/desafio/entities/Cursos.java
@@ -7,6 +7,37 @@ public class Cursos {
 	private String materia;
 	private String coordenador;
 	
-	        
+	public Cursos(int id_curso, String nome_curso, String materia, String coordenador) {
+	
+		this.id_curso = id_curso;
+		this.nome_curso = nome_curso;
+		this.materia = materia;
+		this.coordenador = coordenador;
+	}
+	
+	public int getId_curso() {
+		return id_curso;
+	}
+	public void setId_curso(int id_curso) {
+		this.id_curso = id_curso;
+	}
+	public String getNome_curso() {
+		return nome_curso;
+	}
+	public void setNome_curso(String nome_curso) {
+		this.nome_curso = nome_curso;
+	}
+	public String getMateria() {
+		return materia;
+	}
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+	public String getCoordenador() {
+		return coordenador;
+	}
+	public void setCoordenador(String coordenador) {
+		this.coordenador = coordenador;
+	}       
 
 }
