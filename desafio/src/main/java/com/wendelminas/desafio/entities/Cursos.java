bce77e0ff0f73b69f42bc9afffaf11de1086de29
Wendel Leite da Silva <136202512+wendelminas@users.noreply.github.com>
2023-06-22 10:40:18 -0300

    commit de sincronização com o eclipse

---
+++ desafio/src/main/java/com/wendelminas/desafio/entities/Cursos.java
@@ -0,0 +1,12 @@
+package com.wendelminas.desafio.entities;
+
+public class Cursos {
+	
+	private int id_curso;
+	private String nome_curso;
+	private String materia;
+	private String coordenador;
+	
+	        
+
+}
